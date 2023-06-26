bad6a4868c5c36933e6c9088435ee7f46c922238
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-26 15:55:11 +0100

    finished solutions to event tasks

---
+++ scripts/winGame5.js
@@ -0,0 +1,26 @@
+// add the game address here and update the contract name if necessary
+const gameAddr = "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE";
+const contractName = "Game5";
+
+async function main() {
+    // attach to the game
+    const game = await hre.ethers.getContractAt(contractName, gameAddr);
+
+    
+    await game.giveMeAllowance(30000);
+    await game.mint(20000);
+    // do whatever you need to do to win the game here:
+    const tx = await game.win();
+
+    // did you win? Check the transaction receipt!
+    // if you did, it will be in both the logs and events array
+    const receipt = await tx.wait();
+    console.log(receipt);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
